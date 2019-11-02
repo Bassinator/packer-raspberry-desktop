e4c6cb09b4dc502985c0032d3f98cfb31f128af6
Bastian Bukatz <bastian.greuel@gmail.com>
2019-11-02 07:51:00 +0100

    added buster and 64 bit versions

--- scripts/upgrade.sh
+++ scripts/upgrade.sh
@@ -8,4 +8,6 @@ apt-get install software-properties-common
 apt-add-repository 'deb http://deb.debian.org/debian stretch contrib main non-free'
 
 # Install Ansible dependencies.
-apt-get -y update && apt-get -y dist-upgrade
+apt-get -y update
+apt-get -y dist-upgrade
+# DEBIAN_FRONTEND=noninteractive apt-get -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" dist-upgrade
